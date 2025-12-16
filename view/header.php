0d1c6bf985818ee1357df1e8415bafd5b9fb394a
Your Name <laura.kotlinska.96@gmail.com>
2025-12-16 11:38:21 +0100

    Links for files and change to title

--- view/header.php
+++ view/header.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./../src/styles/style.css">
+    <script src="./../src/scripts/main.js"></script>
+    <title>Terracota Bay Hotel</title>
 </head>
+
 <body>
-<header>
-    <?php require __DIR__ . '/navigation.php' ?>
-</header>
+    <header>
+        <?php require __DIR__ . '/navigation.php' ?>
+    </header>
