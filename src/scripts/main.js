de506a706bedeafde1527986ac49919432da3e8e
Your Name <laura.kotlinska.96@gmail.com>
2025-12-16 15:36:40 +0100

    Adjusting price display due to incorrect form value

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,9 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
   let priceOutput = document.getElementById("price");
 
   function displayPrice() {
-    const roomPrice = roomSelect.value;
-    if (roomPrice) {
-      priceOutput.innerText = roomPrice + " $";
+    const roomId = roomSelect.value;
+    const room = rooms.find((r) => r.id == roomId);
+    if (room) {
+      priceOutput.textContent = room.price_per_night + " $";
     }
   }
 
