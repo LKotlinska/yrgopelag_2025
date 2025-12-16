75459232206e2ac99534b38265d66bd386905ce8
Your Name <laura.kotlinska.96@gmail.com>
2025-12-16 11:32:55 +0100

    Find unoccupied rooms to display in the calender

--- src/backend/calendar.php
+++ src/backend/calendar.php
@@ -1,27 +1,24 @@
 <?php
-
-$database = new PDO('sqlite:' . __DIR__ . '/../database/hotel.db');
-
+// Hardcoded due to task limitation
 $year = 2026;
 $month = 1;
 $day = 1;
 $cells = 42;
+
 $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $month, $year);
-
-
 $firstDayOfMonth = date('N', strtotime("$year-$month-01"));
+$date = sprintf('%04d-01-%02d', $year, $day);
 
-
-
-$availability = [];
-
-if (isset($_POST['selected_date'])) {
-    $selectedDate = $_POST['selected_date'];
-    $query = $database->prepare('SELECT room_id, is_available 
-                                 FROM room_availability 
-                                 WHERE date = :date');
-
-    $query->execute([':date' => $selectedDate]);
-    $availability = $query->fetchAll(PDO::FETCH_ASSOC);
-    print_r($availability);
+function bookedRooms(array $bookings, string $date): array
+{
+    $unavailableRooms = [];
+    foreach ($bookings as $booking) {
+        if (
+            $date >= $booking['arrival_date'] &&
+            $date <  $booking['departure_date']
+        ) {
+            $unavailableRooms[] = [$date => $booking['room_id']];
+        }
+    }
+    return $unavailableRooms;
 }
