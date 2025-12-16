6f8a3d351844280d74add049d7d9c8b56c76b1d8
Your Name <laura.kotlinska.96@gmail.com>
2025-12-16 11:37:10 +0100

    Adding components in index for testing

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
-<?php 
+<?php
 
 declare(strict_types=1);
 
+require __DIR__ . '/src/database/database.php';
+
+require __DIR__ . '/vendor/autoload.php';
+
 require __DIR__ . '/view/header.php';
+
+require __DIR__ . '/view/form.php';
+
+require __DIR__ . '/view/calendar.php';
 
 require __DIR__ . '/view/footer.php';
