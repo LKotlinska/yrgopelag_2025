ddfa763644b4f39fcb6255ab55000dd2f226ae65
Your Name <laura.kotlinska.96@gmail.com>
2025-12-17 13:24:28 +0100

    Move function to functions.php

--- index.php
+++ index.php
@@ -15,16 +15,3 @@ require __DIR__ . '/view/form.php';
 require __DIR__ . '/view/calendar.php';
 
 require __DIR__ . '/view/footer.php';
-
-function calculateRoomPrice($rooms, $roomId, $arrDate, $depDate)
-{
-    $roomPrice = $rooms[$roomId - 1]['price_per_night'];
-
-    list($arrYear, $arrMonth, $arrDay) = explode("-", $arrDate);
-    list($depYear, $depMonth, $depDay) = explode("-", $depDate);
-    $nights = $depDay - $arrDay;
-
-    $total = $roomPrice * $nights;
-
-    return $total;
-}
