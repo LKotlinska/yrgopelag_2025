7b93d44cfa3204455d09189d4c48336ff9d7c8ad
Your Name <laura.kotlinska.96@gmail.com>
2025-12-22 21:18:02 +0100

    Add font, header and styles

--- view/header.php
+++ view/header.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Fraunces:ital,opsz,wght@0,9..144,100..900;1,9..144,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./../src/styles/style.css">
     <script src="./../src/scripts/main.js"></script>
     <title>Terracota Bay Hotel</title>
@@ -11,5 +14,27 @@
 
 <body>
     <header>
-        <?php require __DIR__ . '/navigation.php' ?>
+        <?php
+        // require __DIR__ . '/navigation.php' 
+        ?>
+        <h1 class="menu">MENU</h1>
+        <h1 class="book">BOOK</h1>
+        <div class="overlay-container">
+            <img class="header-overlay" src="assets/images/terracotta-hotel.png" alt="" />
+        </div>
+        <hgroup class="site-logo">
+            <h1>
+                Terracotta bay
+            </h1>
+            <h2 class="logo-sub">
+                Spa
+                <span class="l-symbol">
+                    &
+                </span>
+                Hotel
+            </h2>
+            <span class="slogan">
+                A place to arrive - and let go
+            </span>
+        </hgroup>
     </header>
