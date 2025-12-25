85bd565bcdee313dda5acca8793ad8512b104cc0
Your Name <laura.kotlinska.96@gmail.com>
2025-12-25 20:37:35 +0100

    Link google icons and delete overlay container

--- view/header.php
+++ view/header.php
@@ -7,6 +7,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Fraunces:ital,opsz,wght@0,9..144,100..900;1,9..144,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="./../src/styles/style.css">
     <script src="./../src/scripts/main.js"></script>
     <title>Terracota Bay Hotel</title>
@@ -19,9 +20,7 @@
         ?>
         <h1 class="menu">MENU</h1>
         <h1 class="book">BOOK</h1>
-        <div class="overlay-container">
-            <img class="header-overlay" src="assets/images/terracotta-hotel.png" alt="" />
-        </div>
+        <img class="header-overlay" src="assets/images/terracotta-hotel.png" alt="" />
         <hgroup class="site-logo">
             <h1>
                 Terracotta bay
