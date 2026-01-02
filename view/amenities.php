0a5ed690023b3b290496fce1af9496bc82a38c58
Your Name <laura.kotlinska.96@gmail.com>
2026-01-02 12:01:25 +0100

    Change icon

--- view/amenities.php
+++ view/amenities.php
@@ -5,7 +5,7 @@
     foreach ($amenities as $amenity) :
         if ($amenity['room_id'] === $room['id']) { ?>
             <p class="include-item"><span class="material-symbols-outlined">
-                    check_box
+                    check
                 </span> <?php echo $amenity['name']; ?></p>
     <?php $count++;
             if (!isset($_GET['id']) && $count === 3) {
