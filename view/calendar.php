bc87e32e0f1bd30d8d37ae591b675c956f14c1d2
Your Name <laura.kotlinska.96@gmail.com>
2026-01-02 15:43:49 +0100

    Calender no longer a form

--- view/calendar.php
+++ view/calendar.php
@@ -3,54 +3,48 @@
 $roomId = $_GET['id'];
 
 ?>
-
-<form method="POST">
-    <input type="hidden" name="selected_date" id="selected-date">
-    <input type="hidden" name="arrival_date" id="arrival-date">
-    <input type="hidden" name="departure_date" id="departure-date">
-    <table class="calendar">
-        <thead>
-            <tr>
-                <th>Mon</th>
-                <th>Tue</th>
-                <th>Wed</th>
-                <th>Thu</th>
-                <th>Fri</th>
-                <th>Sat</th>
-                <th>Sun</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <?php
-                $cellCount = 0;
-                for ($cell = 1; $cell <= $cells; $cell++):
-                    // New row every 7 cells
-                    if ($cellCount === 7) {
-                ?>
-            </tr>
-            <tr>
+<table class="calendar">
+    <thead>
+        <tr>
+            <th>Mon</th>
+            <th>Tue</th>
+            <th>Wed</th>
+            <th>Thu</th>
+            <th>Fri</th>
+            <th>Sat</th>
+            <th>Sun</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
             <?php
-                        $cellCount = 0;
-                    }
-                    if ($cell < $firstDayOfMonth || $day > $daysInMonth) { ?>
-                <td class="empty"></td>
-            <?php
-                    } else {
-                        $date = sprintf('%04d-%02d-%02d', $year, $month, $day);
-                        $isBooked = isRoomBooked($bookings, $roomId, $date); ?>
-
-                <td class="day <?= $isBooked ? 'booked' : 'available' ?>"
-                    data-date="<?= $date ?>">
-                    <span class="date"><?= $day ?></span>
-                </td>
+            $cellCount = 0;
+            for ($cell = 1; $cell <= $cells; $cell++):
+                // New row every 7 cells
+                if ($cellCount === 7) {
+            ?>
+        </tr>
+        <tr>
         <?php
-                        $day++;
-                    }
-                    $cellCount++;
-                endfor;
-        ?>
-            </tr>
-        </tbody>
-</form>
+                    $cellCount = 0;
+                }
+                if ($cell < $firstDayOfMonth || $day > $daysInMonth) { ?>
+            <td class="empty"></td>
+        <?php
+                } else {
+                    $date = sprintf('%04d-%02d-%02d', $year, $month, $day);
+                    $isBooked = isRoomBooked($bookings, $roomId, $date); ?>
+
+            <td class="day <?php echo $isBooked ? 'booked' : 'available' ?>"
+                data-date="<?php echo $date ?>">
+                <?php echo $day ?>
+            </td>
+    <?php
+                    $day++;
+                }
+                $cellCount++;
+            endfor;
+    ?>
+        </tr>
+    </tbody>
 </table>
