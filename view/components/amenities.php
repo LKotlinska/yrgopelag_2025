7653796771a5910d185ab0caba0adcbb4cba498b
Your Name <laura.kotlinska.96@gmail.com>
2026-01-05 16:03:44 +0100

    Deletes unused code

--- view/components/amenities.php
+++ view/components/amenities.php
@@ -1,16 +1,11 @@
 <p>Room includes:</p>
 <div>
     <?php
-    $count = 0;
     foreach ($amenities as $amenity) :
         if ($amenity['room_id'] === $room['id']) { ?>
             <p class="include-item"><span class="material-symbols-outlined">
                     check
                 </span> <?php echo $amenity['name']; ?></p>
-    <?php $count++;
-            if (!isset($_GET['id']) && $count === 3) {
-                break;
-            }
-        }
+    <?php }
     endforeach; ?>
 </div>
