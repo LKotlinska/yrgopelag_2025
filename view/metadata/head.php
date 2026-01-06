a6350d14ee00f7353506d03de97eb611ecf46c0a
Your Name <laura.kotlinska.96@gmail.com>
2026-01-06 10:14:03 +0100

    Fix bug with booking.js error - loading on page it's not used

--- view/metadata/head.php
+++ view/metadata/head.php
@@ -10,6 +10,5 @@
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Fraunces:ital,opsz,wght@0,9..144,100..900;1,9..144,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="../src/styles/style.css">
-    <script src="../src/scripts/booking.js"></script>
     <title>Terracota Bay Hotel</title>
 </head>
