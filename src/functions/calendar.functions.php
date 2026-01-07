6ad626155e929f8b1389bd3dbc4ad253bd41d53a
Your Name <laura.kotlinska.96@gmail.com>
2026-01-07 14:42:33 +0100

    Change element h1 to h2

--- src/functions/calendar.functions.php
+++ src/functions/calendar.functions.php
@@ -26,64 +26,62 @@ function renderCalendar(
     array $bookings,
     int $roomId
 ): void { ?>
-    <aside class="calendar-container">
-        <h1>
-            Availability
-        </h1>
-        <table id="calendar">
-            <thead>
-                <tr>
-                    <th>Mon</th>
-                    <th>Tue</th>
-                    <th>Wed</th>
-                    <th>Thu</th>
-                    <th>Fri</th>
-                    <th>Sat</th>
-                    <th>Sun</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <?php
-                    $cellCount = 0;
-                    for ($cell = 1; $cell <= $cells; $cell++):
-                        // New row every 7 cells
-                        if ($cellCount === 7) {
-                    ?>
-                </tr>
-                <tr>
+    <h2>
+        Availability
+    </h2>
+    <table id="calendar">
+        <thead>
+            <tr>
+                <th>Mon</th>
+                <th>Tue</th>
+                <th>Wed</th>
+                <th>Thu</th>
+                <th>Fri</th>
+                <th>Sat</th>
+                <th>Sun</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
                 <?php
-                            $cellCount = 0;
-                        }
-                        if ($cell < $firstDayOfMonth || $day > $daysInMonth) { ?>
-                    <td class="empty"></td>
+                $cellCount = 0;
+                for ($cell = 1; $cell <= $cells; $cell++):
+                    // New row every 7 cells
+                    if ($cellCount === 7) {
+                ?>
+            </tr>
+            <tr>
+            <?php
+                        $cellCount = 0;
+                    }
+                    if ($cell < $firstDayOfMonth || $day > $daysInMonth) { ?>
+                <td class="empty"></td>
 
-                <?php } else {
-                            $date = sprintf('%04d-%02d-%02d', $year, $month, $day);
-                            $isBooked = isRoomBooked($bookings, $roomId, $date); ?>
+            <?php } else {
+                        $date = sprintf('%04d-%02d-%02d', $year, $month, $day);
+                        $isBooked = isRoomBooked($bookings, $roomId, $date); ?>
 
-                    <td class="day <?php echo $isBooked ? 'booked' : 'available' ?>"
-                        data-date="<?php echo $date ?>">
-                        <?php echo $day ?>
-                    </td>
+                <td class="day <?php echo $isBooked ? 'booked' : 'available' ?>"
+                    data-date="<?php echo $date ?>">
+                    <?php echo $day ?>
+                </td>
 
-            <?php $day++;
-                        }
-                        $cellCount++;
-                    endfor; ?>
-                </tr>
-            </tbody>
-        </table>
+        <?php $day++;
+                    }
+                    $cellCount++;
+                endfor; ?>
+            </tr>
+        </tbody>
+    </table>
 
-        <div class="calendar-tip">
-            <div class="tip-items">
-                <div class="tip-item booked"></div>
-                Fully booked
-            </div>
-            <div class="tip-items">
-                <div class="tip-item available"></div>
-                Room available
-            </div>
+    <div class="calendar-tip">
+        <div class="tip-items">
+            <div class="tip-item booked"></div>
+            Fully booked
         </div>
-    </aside>
+        <div class="tip-items">
+            <div class="tip-item available"></div>
+            Room available
+        </div>
+    </div>
 <?php }
