cf298e702c9f272a0d345a79371e01cafb3533ed
Your Name <laura.kotlinska.96@gmail.com>
2026-01-08 16:04:32 +0100

    Altarnate img fade in bookings

--- src/scripts/booking.js
+++ src/scripts/booking.js
@@ -16,11 +16,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
   roomCostDisplay.innerText = roomTotal;
   featureCostDisplay.innerText = featureTotal;
+
   totalCostDisplay.innerText = 0;
+  // ---- Restore dates on reload
+  const arrivalInput = document.getElementById("arrival_date");
+  const departureInput = document.getElementById("departure_date");
 
-  displayFeatureCost();
-  displayRoomPrice();
+  if (arrivalInput.value) {
+    arrivalDate = new Date(arrivalInput.value);
+  }
 
+  if (departureInput.value) {
+    departureDate = new Date(departureInput.value);
+  }
+
+  // ---- Re-apply min departure date if arrival exists
   document
     .getElementById("arrival_date")
     .addEventListener("change", function () {
@@ -49,6 +59,20 @@ document.addEventListener("DOMContentLoaded", () => {
       //   }
       // });
     });
+
+  displayFeatureCost();
+  displayRoomPrice();
+
+  const checkedPayment = document.querySelector(
+    'input[name="payment_method"]:checked'
+  );
+
+  if (
+    document.getElementById("api-key-field") &&
+    document.getElementById("transfer-code-field")
+  ) {
+    updatePaymentFields(checkedPayment.value);
+  }
 
   document
     .getElementById("departure_date")
@@ -101,20 +125,35 @@ document.addEventListener("DOMContentLoaded", () => {
     totalCostDisplay.innerText = finalTotal;
   }
 
-  // ---- Toggle payment method
+  // ---- Toggle payment method fields
+  function updatePaymentFields(selectedValue) {
+    const isApi = selectedValue === "api_key";
+
+    document.getElementById("api-key-field").style.display = isApi
+      ? "flex"
+      : "none";
+
+    document.getElementById("transfer-code-field").style.display = isApi
+      ? "none"
+      : "flex";
+
+    document.getElementById("api_key").required = isApi;
+    document.getElementById("transfer_code").required = !isApi;
+  }
+  // ---- Toggle on change
   document.querySelectorAll('input[name="payment_method"]').forEach((radio) => {
     radio.addEventListener("change", () => {
-      const isApi = radio.value === "api_key";
-
-      document.getElementById("api-key-field").style.display = isApi
-        ? "flex"
-        : "none";
-      document.getElementById("transfer-code-field").style.display = isApi
-        ? "none"
-        : "flex";
-
-      document.getElementById("api_key").required = isApi;
-      document.getElementById("transfer_code").required = !isApi;
+      updatePaymentFields(radio.value);
     });
   });
+
+  /* ---- ALTERNATING IMG */
+  const images = document.querySelectorAll(".booking-img");
+  let current = 0;
+
+  setInterval(() => {
+    images[current].classList.remove("active");
+    current = (current + 1) % images.length;
+    images[current].classList.add("active");
+  }, 6000); // 4 seconds
 });
